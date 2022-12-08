09b53a6c33c16ca86a27523b85233c41c064459b
Faraz SIDDIQUI <fsiddiqui@b113-17.iut.univ-paris8.fr>
2022-12-08 14:57:08 +0100

    fonction movement

--- src/Main_Start.java
+++ src/Main_Start.java
@@ -5,8 +5,8 @@ public class Main_Start {
     public static void main(String[] arg) {
 
         char[][] plateau = Assets.createPlateau(9,9);
-        plateau[0][8] = 'U';
-        plateau[8][0] = 'C';
+        plateau[1][7] = 'U';
+        plateau[7][1] = 'C';
         Assets.printab(plateau);
 
         Moves.Menu();
