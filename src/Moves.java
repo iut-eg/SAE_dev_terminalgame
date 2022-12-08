e08acab2239bac46f82d4e5749cfc3a845461e74
Faraz SIDDIQUI <fsiddiqui@b113-17.iut.univ-paris8.fr>
2022-12-08 15:08:07 +0100

    fhjkdhf

--- src/Moves.java
+++ src/Moves.java
@@ -7,8 +7,7 @@ public class Moves {
         int height_index;
 
         for (int y=0; tab[][] == Jchar)
-        for (int y=; tab[][] != 'X' || tab[][] != Opschar ; y--)
-        {
+        for (int y=0 ; tab[][] != 'X' || tab[][] != Opschar ; y--) {
 
         }
     }
