6b3ea728edfbcc71acc7763ce7b20cecead3be78
Faraz SIDDIQUI <fsiddiqui@b110-12.iut.univ-paris8.fr>
2022-12-13 14:19:30 +0100

    + interface.java (a coller)

--- src/Interface.java
+++ src/Interface.java
@@ -1,27 +1,61 @@
 import java.util.Scanner;
 
+// class s'occupant de L'INTERACE et du MENU du jeu //
+
 public class Interface {
+    public static void main(String[] args) {
+        char[][] plateau = Assets.createPlateau(9,9);
+        plateau[1][7] = 'U';
+        plateau[7][1] = 'C';
+        Assets.printab(plateau);
+        Interface.menu(plateau);
+        menu(plateau);
+    }
 
+    public static void menu (char[][] tab) { // méthode affichant le menu //
 
-    public static void menu (char[][] tab) {
-        int NbPas ;
-        int numJ = 1;
-        char press_enter = '!';
+        int nbDéplacement ; // nombre de déplacement
+        int numJoueur = 1 ; // Numéro du Joueur ( trouer qqch de mieux)
+        int choix ;  // choix du joueur
         Scanner saisie = new Scanner(System.in) ;
+        nbDéplacement = Dice.rollDice();
 
+        System.out.println("Vous avez "+ nbDéplacement +" déplacements restants");
 
-        System.out.println("Joueur"+numJ+": APPUIYEZ SUR ESPACE POUR CONTINUER");
-        while (press_enter != ' '){
-            press_enter = saisie.next().charAt(0);
+        System.out.println("Dans quel direction voulez-vous vous aller ?");
+        int direction = 0 ;
+
+        while ( direction < 1) {
+            System.out.println("1. " + "Descendre");
+            System.out.println("2. " + "Monter");
+            System.out.println("3. " + "aller à Gauche");
+            System.out.println("4. " + "aller à Droite");
+            direction = saisie.nextInt();
+        }
+
+        System.out.println("Vous avez "+ nbDéplacement +" déplacements restants");
+        int nbPas; // nombre de pas dans la direction choisi par le joueur
+        nbPas = saisie.nextInt();
+
+        while ( nbDéplacement > 0 ) {
+
+            while (nbPas < 0 || nbPas > nbDéplacement) {
+                System.out.println("Erreur ! De combien voulez vous vous déplacer ?");
+                nbPas = saisie.nextInt();
+            }
+            nbDéplacement = nbDéplacement - nbPas;
+
 
         }
 
-        NbPas = Dice.rollDice();
-        System.out.println("Vous avez "+NbPas+"déplacements restants");
+/*
+        System.out.println("Joueur"+numJoueur+": APPUIYEZ SUR ESPACE POUR CONTINUER");
+        choix = saisie.next().charAt(0);
+        while (choix < 1 || choix > 10 ) {
+            choix = saisie.next().charAt(0);
+        }
+        */
 
-        System.out.println("De combien voulez vous vous déplacer ?");
-        System.out.println("1. " + "Pour descendre");
-        System.out.println("2. " + "Pour monter");
 
     }
 
--- src/Moves.java
+++ src/Moves.java
@@ -2,13 +2,44 @@
 
 public class Moves {
 
-    public static int moveUP(char[][] tab,   char Jchar, char OpsChar){
-        int height_index;
 
-        for (int y=0; tab[][] == Jchar)
-        for (int y=0 ; tab[][] != 'X' || tab[][] != Opschar ; y--) {
 
+    public static int[] checkYX(char[][] tab, char myChar) {
+        int [] position = new int[2];
+        for (int y = 0; y < tab.length; y++) {
+            for (int x = 0; x < tab[y].length; x++) {
+                if (tab[y][x] == myChar) {
+                    position[0] = y;
+                    position[1] = x;
+                }
+            }
         }
+        return position;
+    }
+
+    public static int moveUP(char[][] tab, int moves, char FriendlyChar, char OpsChar){
+        int[] positionYX = checkYX(tab, FriendlyChar);
+        int X= positionYX[1];
+        tab[3][1] = OpsChar;
+        tab[4][1] = (char)(OpsChar +32);
+
+        for (int y=(positionYX[0]-1); moves > 0 && y > 0 && tab[y][X] != OpsChar ; y--, moves--)
+        {
+
+
+            if (tab[y][X]== '-'){
+                tab[y][X] = (char)(FriendlyChar + 32);
+            }
+
+            else if (tab[y][X]== (char)(OpsChar +32)){
+                tab[y][X] = '-';
+            }
+            Assets.printab(tab);
+            System.out.println(y+"\n");
+        }
+
+        return moves;
+
     }
 
 
